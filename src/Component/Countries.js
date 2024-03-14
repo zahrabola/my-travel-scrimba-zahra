07f672b7a2ea68d0aef822985de352084736e5f4
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-03-14 12:00:38 +0000

    update

--- src/Component/Countries.js
+++ src/Component/Countries.js
@@ -3,7 +3,25 @@ import React from 'react';
 const Countries = () => {
     return (
         <div>
-            countries
+            <div className='countrycontainer'>
+                <div className='leftcontainer'>
+                    img
+                </div>
+                <div className='rightcontainer'>
+                    <div className='country'>
+                        img
+                        <span className='city'>city</span>
+                        <a className='maplink'>View on Google Maps</a>
+                    </div>
+                    <div className='heading'>
+                        <h1 className='title'>title</h1>
+                    </div>
+                    <div className='content'>
+                        <h4> date - end date</h4>
+                        <p>description</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
